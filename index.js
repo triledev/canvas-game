457a4a8748e0ad5ec4db627a2084ad386cd8271f
Tri Le <tri.le.mail@gmail.com>
2023-10-25 20:09:05 +0700

    implement draw()

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
 
 canvas.width = innerWidth
 canvas.height = innerHeight
@@ -11,6 +12,16 @@ class Player {
         this.radius = radius
         this.color = color
     }
+
+    draw() {
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.color
+        c.fill()
+    }
 }
 
-const player = new Player()
+const player = new Player(100, 100, 30, 'blue')
+player.draw()
+
+console.log(player)
