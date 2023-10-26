450d563243423bdb0eefc0be89c5fd0ed01353ad
Tri Le <tri.le.mail@gmail.com>
2023-10-26 11:19:18 +0700

    enemies of random size 0 to 30

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ const enemies = []
 
 function spawnEnemies() {
     setInterval(() => {
-        const radius = 30
+        const radius = Math.random() * 30
         let x
         let y 
         let color
