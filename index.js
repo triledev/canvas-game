1c0fab9de450fb518437fc19461f46f266a94c9c
Tri Le <tri.le.mail@gmail.com>
2023-10-26 10:33:58 +0700

    player shoots particles

--- index.js
+++ index.js
@@ -48,7 +48,6 @@ class Projectile {
 const x = canvas.width / 2
 const y = canvas.height / 2
 const player = new Player(x, y, 30, 'blue')
-player.draw()
 
 const projectile = new Projectile(
     canvas.width/2, 
@@ -66,13 +65,23 @@ const projectiles = [projectile, projectile2]
 
 function animate() {
     requestAnimationFrame(animate)
+    c.clearRect(0, 0, canvas.width, canvas.height)
+    player.draw()
     projectiles.forEach((projectile) => {
         projectile.update()
     })
 }
 
 console.log(player)
-window.addEventListener('click', (event) => {
+addEventListener('click', (event) => {
+    const angle = Math.atan2(event.clientY - canvas.height/2, event.clientX - canvas.width/2)
+    const velocity = {
+        x: Math.cos(angle),
+        y: Math.sin(angle)
+    }
+    projectiles.push(
+        new Projectile(canvas.width/2, canvas.height/2, 5, 'red', velocity)
+    )
     console.log(event)
 
 })
