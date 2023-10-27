3486d2e35b163ed9309fe3d9291e6bf2129c1802
Tri Le <tri.le.mail@gmail.com>
2023-10-27 14:59:25 +0700

    add game start capability

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const scoreEl = document.querySelector('#scoreEl')
 const modalEl = document.querySelector('#modalEl')
 const modalScoreEl = document.querySelector('#modalScoreEl')
 const buttonEl = document.querySelector('#buttonEl')
+const startButtonEl = document.querySelector('#startButtonEl')
+const startModalEl = document.querySelector('#startModalEl')
 
 canvas.width = innerWidth
 canvas.height = innerHeight
@@ -256,5 +258,9 @@ buttonEl.addEventListener('click', (event) => {
     modalEl.style.display = 'none'
 })
 
-animate()
-spawnEnemies()
+startButtonEl.addEventListener('click', () => {
+    init()
+    animate()
+    spawnEnemies()
+    startModalEl.style.display = 'none'
+})
