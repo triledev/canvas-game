0f45e45c0a5b8e1b194b0736ce40fb53ee1c2a01
Tri Le <tri.le.mail@gmail.com>
2023-10-28 18:48:09 +0700

    implement Homing enemy

--- index.js
+++ index.js
@@ -85,6 +85,11 @@ class Enemy {
         this.radius = radius 
         this.color = color
         this.velocity = velocity
+        this.type = 'Linear'
+
+        if (Math.random() < 0.5) {
+            this.type = 'Homing'
+        }
     }
 
     draw() {
@@ -96,9 +101,15 @@ class Enemy {
 
     update() {
         this.draw()
+
+        if (this.type == 'Homing') {
+            const angle = Math.atan2(player.y - this.y, player.x - this.x)
+            this.velocity.x = Math.cos(angle)
+            this.velocity.y = Math.sin(angle)
+        }
+
         this.x = this.x + this.velocity.x
         this.y = this.y + this.velocity.y 
-
     }
 }
 
