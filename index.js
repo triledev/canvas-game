b43bcada82ab09f4474081fb2622609f7d844625
Tri Le <tri.le.mail@gmail.com>
2023-10-28 19:39:08 +0700

    Homming and Spinning enemy

--- index.js
+++ index.js
@@ -85,19 +85,20 @@ class Enemy {
         this.radius = radius 
         this.color = color
         this.velocity = velocity
-        this.type = 'Linear'
+        this.type = 'Homing Spinning'
+        // this.type = 'Linear'
         this.radians = 0
         this.center = {
             x,
             y
         }
 
-        if (Math.random() < 0.5) {
-            this.type = 'Homing'
-            if (Math.random() < 0.5) {
-                this.type = 'Spinning'
-            }
-        }
+        // if (Math.random() < 0.5) {
+        //     this.type = 'Homing'
+        //     if (Math.random() < 0.5) {
+        //         this.type = 'Spinning'
+        //     }
+        // }
     }
 
     draw() {
@@ -110,7 +111,7 @@ class Enemy {
     update() {
         this.draw()
 
-        if (this.type == 'Spinning') {
+        if (this.type === 'Spinning') {
             this.radians += 0.1
             // the center targeting player
             this.center.x += this.velocity.x
@@ -119,7 +120,7 @@ class Enemy {
             // actual reanderd position
             this.x = this.center.x + Math.cos(this.radians) * 30
             this.y = this.center.y + Math.sin(this.radians) * 30
-        } else if (this.type == 'Homing') {
+        } else if (this.type === 'Homing') {
             // calculate the angle to target the player
             const angle = Math.atan2(player.y - this.y, player.x - this.x)
             this.velocity.x = Math.cos(angle)
@@ -127,6 +128,19 @@ class Enemy {
 
             this.x = this.x + this.velocity.x
             this.y = this.y + this.velocity.y 
+        } else if (this.type === 'Homing Spinning') {
+            this.radians += 0.1
+            // the center targeting player
+            const angle = Math.atan2(player.y - this.center.y, player.x - this.center.x)
+            this.velocity.x = Math.cos(angle)
+            this.velocity.y = Math.sin(angle)
+
+            this.center.x += this.velocity.x
+            this.center.y += this.velocity.y 
+            
+            // actual reanderd position
+            this.x = this.center.x + Math.cos(this.radians) * 30
+            this.y = this.center.y + Math.sin(this.radians) * 30
         } else {
             this.x = this.x + this.velocity.x
             this.y = this.y + this.velocity.y 
